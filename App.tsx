aa37410694a1eb01dd0cc6e3cf5ef0f1a7d9a5d5
Johndoe <56298669+togane@users.noreply.github.com>
2023-03-19 14:41:01 +0900

    おおまかなレイアウトを作成

--- App.tsx
+++ App.tsx
@@ -1,11 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+  const [values, setValues] = useState<string[]>(Array(8).fill('0'));
+
+  const updateValue = (index: number, value: string) => {
+    if (value === '0' || value === '1') {
+      const newValues = [...values];
+      newValues[index] = value;
+      setValues(newValues);
+    }
+  };
+
+  const decimalValue = parseInt(values.join(''), 2);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>{decimalValue}</Text>
+      <View style={styles.boxContainer}>
+        {values.map((value, index) => (
+          <TextInput
+            key={index}
+            style={styles.box}
+            value={value}
+            maxLength={1}
+            keyboardType="number-pad"
+            onChangeText={(text) => updateValue(index, text)}
+          />
+        ))}
+      </View>
     </View>
   );
 }
@@ -16,5 +39,20 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+  boxContainer: {
+    flexDirection: 'row',
+  },
+  box: {
+    width: 40,
+    height: 40,
+    borderWidth: 1,
+    borderColor: 'black',
+    textAlign: 'center',
+    fontSize: 18,
   },
 });
